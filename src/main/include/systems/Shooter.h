23a2fb9acebfaf62f283abae6eedb964d70dc00f
turtleman325 <148637523+turtleman325@users.noreply.github.com>
2026-03-06 18:37:45 -0500

    Add files via upload

--- src/main/include/systems/Shooter.h
+++ src/main/include/systems/Shooter.h
@@ -1,54 +1,70 @@
 #pragma once
 
+// Venderdep includes
 #include <rev/SparkMax.h>
 #include <rev/config/SparkMaxConfig.h>
 #include <ctre/phoenix6/TalonFX.hpp>
 #include <ctre/phoenix6/controls/VelocityVoltage.hpp>
 #include <frc/Servo.h>
+#include <units/angle.h>
+#include <units/angular_velocity.h>
 
+// subsystem includes
 #include "Constants.h"
 #include "System.h"
 
+// shooter class definition
 class Shooter : public System {
-
 public:
+    // singleton getinstance
     static Shooter &GetInstance() {
         static Shooter instance;
         return instance;
     }
 
-    void SetAngle(double angle);
+    // Shooter State
+    enum shooterStates {IDLE, PREFIRE, FIRE};
 
-    void SetShooterSpeed(units::angular_velocity::turns_per_second_t Speed);
+    // Setters
+    void SetTurretAngle(units::degree_t angle);
+    void SetShooterSpeed(units::turns_per_second_t speed);
+    void SetHoodPosition(double position);
+    void StartShooting();
+    void StopShooting();
 
-    bool IsAtSetpoint();
+    // Getters
+    double GetTurretAngle();
+    double GetShooterSpeed();
+    double GetHoodPosition();
+    shooterStates GetShooterState();
 
+    // other functions
     void Update(Robot::Mode mode, double t) override;
-
-    void SetHoodState(double state);
+    bool ReadyToFire();
 
 private:
-    // change back to Turret motor for once done
+    // turret hardware
     rev::spark::SparkMax m_azimuthTurretMotor{Constants::kAzimuthMotorId, rev::spark::SparkMax::MotorType::kBrushless}; // get motor for turret rotation
     rev::spark::SparkClosedLoopController m_azimuthController = m_azimuthTurretMotor.GetClosedLoopController(); // get controller for turret rotation
-
-    ctre::phoenix6::hardware::TalonFX m_shooterMotorRight{Constants::kShooterMotorRightId};
-    ctre::phoenix6::hardware::TalonFX m_shooterMotorLeft{Constants::kShooterMotorLeftId};
-
-    // control templates for velocity control on shooter motors
-    ctre::phoenix6::controls::VelocityVoltage m_rightVelocity{0.0_rpm};
-    ctre::phoenix6::controls::VelocityVoltage m_leftVelocity{0.0_rpm};
-
-    frc::Servo m_leftHoodServo{Constants::kLeftHoodServoPort};
-    frc::Servo m_rightHoodServo{Constants::kRightHoodServoPort};
-
     rev::spark::SparkMaxConfig m_azimuthConfig; //configured in the cpp file
 
-    units::angular_velocity::turns_per_second_t m_shooterSpeed;
+    // shooter hardware
+    ctre::phoenix6::hardware::TalonFX m_shooterMotorRight{Constants::kShooterMotorRightId}; // Kraken X60 #1
+    ctre::phoenix6::hardware::TalonFX m_shooterMotorLeft{Constants::kShooterMotorLeftId}; // Kraken X60 #2
 
-    double m_azimuthSetpoint;
+    ctre::phoenix6::controls::VelocityVoltage m_rightVelocity = ctre::phoenix6::controls::VelocityVoltage{0_tps}.WithSlot(0); // right motor velocity voltage controller
+    ctre::phoenix6::controls::VelocityVoltage m_leftVelocity = ctre::phoenix6::controls::VelocityVoltage{0_tps}.WithSlot(0); // left motor velocity voltage controller
+    ctre::phoenix6::controls::NeutralOut m_brake{};
 
-    double m_hoodState;
+    frc::Servo m_leftHoodServo{Constants::kLeftHoodServoPort}; // Hood linear servo #1
+    frc::Servo m_rightHoodServo{Constants::kRightHoodServoPort}; // Hood linear servo #2
 
+    // member variables
+    shooterStates m_shooterState;
+    units::degree_t m_azimuthSetpoint; // turret angle setpoint (degrees)
+    units::turns_per_second_t m_velocitySetpoint; // shooter flywheel speed setpoint (RPS)
+    double m_hoodSetpoint; // hood angle setpoint (% output, 0-1)
+
+    // Singleton private constructor
     Shooter();
 };
