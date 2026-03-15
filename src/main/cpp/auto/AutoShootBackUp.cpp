d52ddfe9f692fa01b969f0cb61ef15258322c7d3
turtleman325 <matttheturtleman@gmail.com>
2026-03-14 23:32:06 -0400

    uhhh back up and shoot it works now (probably (hopefullu

--- src/main/cpp/auto/AutoShootBackUp.cpp
+++ src/main/cpp/auto/AutoShootBackUp.cpp
@@ -7,12 +7,12 @@
 
 
 AutoShootBackUp::AutoShootBackUp(frc::DriverStation::Alliance alliance) {
-    // frc::Pose2d pose = SwerveDrive::GetInstance().GetPose2d();
-    // m_tasks.push_back(std::make_shared<FollowPath>(
-    //     std::vector<frc::Pose2d>{
-    //         SwerveDrive::GetInstance().GetPose2d(),
-    //         //transform ts like 3 or 4 feet backwards
-    //         SwerveDrive::GetInstance().GetPose2d().TransformBy(frc::Transform2d{}),   
-    // false, false));
-    // m_tasks.push_back(std::make_shared<StartShooter>());
+    frc::Pose2d pose = SwerveDrive::GetInstance().GetPose2d();
+    m_tasks.push_back(std::make_shared<FollowPath>(
+        std::vector<frc::Pose2d>{
+            SwerveDrive::GetInstance().GetPose2d(),
+            //transform ts like 3 or 4 feet backwards
+            SwerveDrive::GetInstance().GetPose2d().TransformBy(frc::Transform2d{2_m, 0_m, 0_deg})},   
+    false, false));
+    m_tasks.push_back(std::make_shared<StartShooter>());
 }
