29a3b03139b136c7fdc969358ff38dc1f63ff8c6
turtleman325 <matttheturtleman@gmail.com>
2026-03-19 17:16:25 -0400

    more things that might work

--- src/main/cpp/auto/AutoCenter.cpp
+++ src/main/cpp/auto/AutoCenter.cpp
@@ -36,10 +36,9 @@ AutoCenter::AutoCenter(frc::DriverStation::Alliance alliance, bool onLeft) {
       SwerveDrive::GetInstance().GetPose2d(),
       frc::Pose2d{
         units::meter_t{(alliance == 0 ? Constants::kFieldLength - (0.5 * Constants::kStartLineOffset) : 0.5 * Constants::kStartLineOffset)},
-        units::meter_t{(Constants::kFieldWidth / 2) + ((m_onTop == 0 ? -1 : 1) * Constants::kStartOffsetY)},
+        units::meter_t{(Constants::kFieldWidth / 2) + ((!m_onTop ? -1 : 1) * Constants::kStartOffsetY)},
         (alliance == 0 ? 180_deg : 0_deg)
-      }},
-  false, false));
+      }}, false, false));
 
   // shoot preloaded balls
   m_tasks.push_back(std::make_shared<StartShooter>());
@@ -53,7 +52,7 @@ AutoCenter::AutoCenter(frc::DriverStation::Alliance alliance, bool onLeft) {
       frc::Pose2d{
         units::meter_t{alliance == 0 ? Constants::kFieldLength : 0} 
           + ((alliance == 0 ? -1 : 1) * units::meter_t{Constants::kStartLineOffset + (Constants::kRobotWidth / 2)} + 44.40_in),
-        units::meter_t{(Constants::kFieldWidth / 2) + ((m_onTop == 0 ? -1 : 1) * Constants::kStartOffsetY)}, 
+        units::meter_t{(Constants::kFieldWidth / 2) + ((!m_onTop ? -1 : 1) * Constants::kStartOffsetY)}, 
         (alliance == 0 ? 180_deg : 0_deg)
       }}, false, false));
   
@@ -65,7 +64,7 @@ AutoCenter::AutoCenter(frc::DriverStation::Alliance alliance, bool onLeft) {
       frc::Pose2d{
         units::meter_t{Constants::kFieldLength / 2} + (alliance == 0 ? 17.975_in : -17.975_in), 
         (m_onTop ? 0_m + 25.62_in : (units::meter_t{Constants::kFieldWidth} - 25.62_in)), 
-        (m_onTop ? -90_deg : 90_deg)
+        (!m_onTop ? 1 : -1) * 90_deg
       }}, false, false));
   
   // starts intake & moves forward to collect balls
@@ -77,8 +76,8 @@ AutoCenter::AutoCenter(frc::DriverStation::Alliance alliance, bool onLeft) {
       // robot returns to starting y position, aligning with the ramp
       frc::Pose2d{
         units::meter_t{Constants::kFieldLength / 2} + (alliance == 0 ? 17.975_in : -17.975_in),
-        units::meter_t{(Constants::kFieldWidth / 2) + ((m_onTop == 0 ? -1 : 1) * Constants::kStartOffsetY)}, 
-        (m_onTop ? -90_deg : 90_deg)
+        units::meter_t{(Constants::kFieldWidth / 2) + ((!m_onTop ? -1 : 1) * Constants::kStartOffsetY)}, 
+        (!m_onTop ? 1 : -1) * 90_deg
       }}, false, false));
   
   // give time for intake to finish intaking (todo: tune probably. dont let this be too long)
@@ -92,7 +91,7 @@ AutoCenter::AutoCenter(frc::DriverStation::Alliance alliance, bool onLeft) {
       SwerveDrive::GetInstance().GetPose2d(),
       frc::Pose2d{
         units::meter_t{Constants::kFieldLength / 2} + (alliance == 0 ? 17.975_in : -17.975_in),
-        units::meter_t{(Constants::kFieldWidth / 2) + ((m_onTop == 0 ? -1 : 1) * Constants::kStartOffsetY)},  
+        units::meter_t{(Constants::kFieldWidth / 2) + ((!m_onTop ? -1 : 1) * Constants::kStartOffsetY)},  
         (alliance == 0 ? 180_deg : 0_deg)
       }}, false, false));
 
@@ -102,9 +101,91 @@ AutoCenter::AutoCenter(frc::DriverStation::Alliance alliance, bool onLeft) {
       SwerveDrive::GetInstance().GetPose2d(),
       frc::Pose2d{
         units::meter_t{(alliance == 0 ? Constants::kFieldLength - (0.5 * Constants::kStartLineOffset) : 0.5 * Constants::kStartLineOffset)},
-        units::meter_t{(Constants::kFieldWidth / 2) + ((m_onTop == 0 ? -1 : 1) * Constants::kStartOffsetY)},
+        units::meter_t{(Constants::kFieldWidth / 2) + ((!m_onTop ? -1 : 1) * Constants::kStartOffsetY)},
         (alliance == 0 ? 180_deg : 0_deg)
-      }},
-  false, false));
+      }}, false, false));
   m_tasks.push_back(std::make_shared<StartShooter>());
+  
+
+  // same thing but using a rotation around the center of the field to do the red side
+  // // back up to make shooter work
+  // m_tasks.push_back(std::make_shared<FollowPath>(
+  //   std::vector<frc::Pose2d>{
+  //     SwerveDrive::GetInstance().GetPose2d(),
+  //     frc::Pose2d{
+  //       units::meter_t{0.5 * Constants::kStartLineOffset},
+  //       units::meter_t{(Constants::kFieldWidth / 2) + ((onLeft ? -1 : 1) * Constants::kStartOffsetY)},
+  //       0_deg
+  //     }.RotateAround(Constants::kFieldCenter, (alliance == 0 ? 180_deg : 0_deg))},
+  // false, false));
+
+  // // shoot preloaded balls
+  // m_tasks.push_back(std::make_shared<StartShooter>());
+  // m_tasks.push_back(std::make_shared<Delay>(2.0));  
+  // m_tasks.push_back(std::make_shared<StopShooter>());
+  
+  // // goes to a position half a robot width past the edge of the ramp
+  // m_tasks.push_back(std::make_shared<FollowPath>(
+  //   std::vector<frc::Pose2d>{
+  //     SwerveDrive::GetInstance().GetPose2d(), 
+  //     frc::Pose2d{
+  //       units::meter_t{Constants::kStartLineOffset + (Constants::kRobotWidth / 2)} + 44.40_in,
+  //       units::meter_t{(Constants::kFieldWidth / 2) + ((onLeft ? -1 : 1) * Constants::kStartOffsetY)}, 
+  //       0_deg
+  //     }.RotateAround(Constants::kFieldCenter, (alliance == 0 ? 180_deg : 0_deg))}, 
+  // false, false));
+  
+  // // moves the robot above/below the balls & aligns it
+  // m_tasks.push_back(std::make_shared<FollowPath>(
+  //   std::vector<frc::Pose2d>{
+  //     SwerveDrive::GetInstance().GetPose2d(),
+  //     // x value is the middle of the field +- 1/4 length of the fuel rectangle. 
+  //     frc::Pose2d{
+  //       units::meter_t{Constants::kFieldLength / 2} - 17.975_in, 
+  //       (onLeft ? (units::meter_t{Constants::kFieldWidth} - 25.62_in) : 0_m + 25.62_in), 
+  //       (onLeft ? 1 : -1) * 90_deg
+  //     }.RotateAround(Constants::kFieldCenter, (alliance == 0 ? 180_deg : 0_deg))}, 
+  // false, false));
+  
+  // // starts intake & moves forward to collect balls
+  // m_tasks.push_back(std::make_shared<ToggleIntake>());
+  // m_tasks.push_back(std::make_shared<StartIntake>());
+  // m_tasks.push_back(std::make_shared<FollowPath>(
+  //   std::vector<frc::Pose2d>{
+  //     SwerveDrive::GetInstance().GetPose2d(),
+  //     // robot returns to starting y position, aligning with the ramp
+  //     frc::Pose2d{
+  //       units::meter_t{Constants::kFieldLength / 2} - 17.975_in,
+  //       units::meter_t{(Constants::kFieldWidth / 2) + ((onLeft ? -1 : 1) * Constants::kStartOffsetY)}, 
+  //       (onLeft ? 1 : -1) * 90_deg
+  //     }.RotateAround(Constants::kFieldCenter, (alliance == 0 ? 180_deg : 0_deg))}, 
+  // false, false));
+  
+  // // give time for intake to finish intaking (todo: tune probably. dont let this be too long)
+  // m_tasks.push_back(std::make_shared<Delay>(0.5));
+  
+  // // stop intaking balls, rotate to face ramp
+  // m_tasks.push_back(std::make_shared<StopIntake>());
+  // m_tasks.push_back(std::make_shared<ToggleIntake>());
+  // m_tasks.push_back(std::make_shared<FollowPath>(
+  //   std::vector<frc::Pose2d>{
+  //     SwerveDrive::GetInstance().GetPose2d(),
+  //     frc::Pose2d{
+  //       units::meter_t{Constants::kFieldLength / 2} - 17.975_in,
+  //       units::meter_t{(Constants::kFieldWidth / 2) + ((onLeft ? -1 : 1) * Constants::kStartOffsetY)},  
+  //       0_deg
+  //     }.RotateAround(Constants::kFieldCenter, (alliance == 0 ? 180_deg : 0_deg))}, 
+  // false, false));
+
+  // // go over ramp and start shooting balls
+  // m_tasks.push_back(std::make_shared<FollowPath>(
+  //   std::vector<frc::Pose2d>{
+  //     SwerveDrive::GetInstance().GetPose2d(),
+  //     frc::Pose2d{
+  //       units::meter_t{0.5 * Constants::kStartLineOffset},
+  //       units::meter_t{(Constants::kFieldWidth / 2) + ((onLeft ? -1 : 1) * Constants::kStartOffsetY)},
+  //       0_deg
+  //     }.RotateAround(Constants::kFieldCenter, (alliance == 0 ? 180_deg : 0_deg))},
+  // false, false));
+  // m_tasks.push_back(std::make_shared<StartShooter>());
 }
