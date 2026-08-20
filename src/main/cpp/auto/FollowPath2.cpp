2c568628727d760fc690185255cba6b8a013d5c4
mining-accident <teddyfischer8@gmail.com>
2026-08-20 18:41:03 -0400

    WIP followPath2 update with new path follower

--- src/main/cpp/auto/FollowPath2.cpp
+++ src/main/cpp/auto/FollowPath2.cpp
@@ -10,26 +10,41 @@ FollowPath2::FollowPath2(std::vector<std::tuple<frc::Pose2d, double, double, dou
     m_persist{persist} {}
 
 void FollowPath2::Start(double t) {
+    // Reset pose and drivetrain controllers
     if (m_resetPose && m_points.size() > 0) {
         SwerveDrive::GetInstance().ResetPose(std::get<0>(m_points[0]));
+        SwerveDrive::GetInstance().RestControllers();
     }
 
+    // Advance point index
     if (m_points.size() > 1) {
         m_pointIndex++;
     }
 
+    // Start path
     m_started = true;
 }
 
 void FollowPath2::Update(double t) {
-    // if (AtPoint() && m_pointIndex < m_points.size() - 1) {
-    //     m_pointIndex++;
-    // }
+    // Advance point index when at point
+    if (SwerveDrive::GetInstance().AtPositionSetpoint() && m_pointIndex < m_points.size() - 1) {
+        m_pointIndex++;
+    }
+
+    // Command drivetrain
+    SwerveDrive::GetInstance().DriveToPose(std::get<0>(m_points[m_pointIndex]), 
+                                           std::get<0>(m_points[m_pointIndex + 1]), 
+                                           std::get<0>(m_points[m_pointIndex + 2]), 
+                                           std::get<1>(m_points[m_pointIndex + 1]), 
+                                           std::get<2>(m_points[m_pointIndex + 1]));
+
+
 }
 
 void FollowPath2::Stop() {
-    // SwerveDrive::GetInstance().DriveVelocity(0, 0, 0);
-    // //SwerveDrive::GetInstance().EnableRamp();
+    if (!m_persist) {
+        SwerveDrive::GetInstance().DriveVelocity(0, 0, 0);
+    }
 }
 
 bool FollowPath2::IsDone() const {
